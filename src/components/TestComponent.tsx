ff055e24bd5e05a93e393a1d8796af07c34100a9
Jarosław Wasiak <jaroslaw.wasiak@rst.com.pl>
2022-11-29 16:47:36 +0000

    changed package name, removed  unnecessary files

--- src/components/TestComponent.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react'
-import { BasePropertyProps } from 'adminjs'
-
-const TestComponent: FC<BasePropertyProps> = () => {
-  return <p>This is test.</p>
-}
-
-export default TestComponent
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { bundle } from './bundle'
 
-const TestComponent = bundle('TestComponent')
 const ReferenceField = bundle('ReferenceField')
 
-export { TestComponent, ReferenceField }
+export { ReferenceField }
